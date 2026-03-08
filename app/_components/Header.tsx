bb435cd8cd7aa881f9fd5b5687febc043c906f8c
Mohd Ashif <mohdashif0770@gmail.com>
2026-03-08 15:31:11 +0530

    auth added with cleark

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,8 +16,7 @@ import {
 import { UserButton, useUser } from "@clerk/nextjs";
 
 export const Header = () => {
-  const { user } = useUser();
-  console.log(user);
+  const { isSignedIn } = useUser();
   return (
     <div className="px-18 py-4 max-w-7xl mx-auto flex justify-between items-center w-full">
       <div className="flex items-center gap-2">
@@ -30,18 +29,18 @@ export const Header = () => {
           <NavigationMenuItem>
             <NavigationMenuTrigger>Courses</NavigationMenuTrigger>
             <NavigationMenuContent>
+              
               <ul className="grid md:grid-cols-2 sm:w-[400px] md:w-[500px] lg:w-[600px] gap-2">
                 {courses.map((course, index) => (
-                  <div
+                  <li
                     key={index}
                     className="p-2 hover:bg-accent rounded-md cursor-pointer"
                   >
                     <h3 className="font-medium">{course.name}</h3>
                     <p className="text-sm text-gray-500">{course.desc}</p>
-                  </div>
+                  </li>
                 ))}
-              </ul>
-            </NavigationMenuContent>
+              </ul>            </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
@@ -68,18 +67,25 @@ export const Header = () => {
       </NavigationMenu>
 
       {/* sign up button */}
-      {!user ? (
+      {!isSignedIn ? (
         <Link href="/sign-up">
-        <Button className="font-game text-xl cursor-pointer" variant="pixel">
-          Sign Up
-        </Button>
+          <Button className="font-game text-xl cursor-pointer" variant="pixel">
+            Sign Up
+          </Button>
         </Link>
       ) : (
         <div className="flex items-center gap-4">
-          <Button className="font-game text-xl cursor-pointer" variant="pixel">
-            Dashboard
-          </Button>
-          <UserButton />
+          <div className="flex items-center gap-4">
+            <Link href="/dashboard">
+              <Button
+                className="font-game text-xl cursor-pointer"
+                variant="pixel"
+              >
+                Dashboard
+              </Button>
+            </Link>
+            <UserButton />
+          </div>{" "}
         </div>
       )}
     </div>
